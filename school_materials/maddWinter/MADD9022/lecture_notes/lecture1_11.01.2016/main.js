8d8f832745393a54903398badc8c99e4f9066285
anafora <minkim4568>
2016-01-13 00:13:56 -0500

    fief
    fefe

---
+++ school_materials/maddWinter/MADD9022/lecture_notes/lecture1_11.01.2016/main.js
@@ -0,0 +1,78 @@
+///**
+// * Created by Min on 2016-01-11.
+// */
+//
+////var mol = 42;
+////var result = func(mol);
+//
+//// func does not exist
+//
+//try{
+//    //do something
+//    var mol = 42;
+//    var result = func(mol);
+//
+//    if(result > 0)
+//        doA();
+//    else
+//        doB(); // this is shortcut if there is only one line.
+//               // If professionally written, not recommended
+//               // because if someone writes something on top of it and forget to put {},
+//               // then it goes wrong.
+//}
+//catch(e) {
+//    //console.log("Name: " + e.name + " Message: " + e.message);
+//    console.log(e);
+//}
+//finally {
+//    // will run no matter what
+//
+//}
+//
+////message is better for debugging
+
+//===========
+//
+//
+//var userData = {
+//    "firstName" : "Eric",
+//    "lastName" : "Kim",
+//    "email": "kim@gmail.com"
+//
+//
+//}
+//
+//userData.today = new Date)_.toDateString();
+//var data = JSON.stringify(userData);
+//var bata = userData;
+//
+//function checkJasonData(jsonData) {
+//    try {
+//        data = JSON.parse(jsonData);
+//    }
+//    catch(e) {
+//        document.getElementsByName("siteTitle");
+//        data = null; //if try block fails, then data becomes "null".
+//    }
+//    finally {
+//        var img;
+//        if(data != null) {
+//
+//        }
+//
+//    }
+//}
+
+// the point is that the website is still working even if my code goes wrong at some part.
+// any file operation will use try and catch block because any of it can fail.
+
+
+
+
+var s = "text";
+var n = 42;
+var b = true;
+
+console.info(typeof s);
+
+// Tony told me that he is going to provide us with the source code in the next class
